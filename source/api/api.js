bed4d68778eec7124be435b4907fda74d6d86ef1
Kyle1668 <kyledevinobrien1>
2017-06-20 20:59:39 -0700

    Implimented auto complete search bar

--- source/api/api.js
+++ source/api/api.js
@@ -1,5 +1,14 @@
 
 $(document).ready(function () {
 	// Autocomplete search bar functionality.
-	new google.maps.places.Autocomplete(document.getElementById("searchBar"));
+	var placeSearch = new google.maps.places.Autocomplete(document.getElementById("searchBar"));
+
+	google.maps.event.addListener(placeSearch, 'place_changed', function() {
+		var place = placeSearch.getPlace();
+		var request = "https://" + "maps.googleapis.com/maps/api/place/textsearch/xml?query=Coffee+near+" + place.formatted_address + "&key=AIzaSyC_KZyErDtZ42CuFscO2l5YseWaV8MCHrQ";
+		console.log(place.formatted_address);
+		console.log(request);
+		console.log(place.result);
+	})
+
 });
