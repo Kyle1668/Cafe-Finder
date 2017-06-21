77a23a72322950d3aedaa3bee550c08d5cbb6141
Kyle1668 <kyledevinobrien1>
2017-06-20 22:37:33 -0700

    progress

--- source/api/api.js
+++ source/api/api.js
@@ -5,15 +5,17 @@ $(document).ready(function () {
 
 	google.maps.event.addListener(placeSearch, 'place_changed', function() {
 		var place = placeSearch.getPlace();
-		var request = `https://" + "maps.googleapis.com/maps/api/place/textsearch/xml?query=Coffee+near+"
-							+ ${place.formatted_address} + "&key=AIzaSyC_KZyErDtZ42CuFscO2l5YseWaV8MCHrQ&sensor=f`;
 
-		$.getJSON(request, function () {
-			console.log("worked!");
+		var get = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=Coffee+near+" +  place.formatted_address +  "&key=AIzaSyC_KZyErDtZ42CuFscO2l5YseWaV8MCHrQ&sensor=false";
+
+		$.get(get, function (data) {
+			console.log("Worked!");
+			console.log(data);
+			$("h1").text(data.results[1].name);
 		});
 
 		console.log(place.formatted_address);
-		console.log(request);
+		console.log(get);
 		console.log(place.result);
 	})
 
