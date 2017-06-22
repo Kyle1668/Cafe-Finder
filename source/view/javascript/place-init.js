b60bdb9df9d3b90af925eeb82df5453da59d9f9e
Kyle1668 <kyledevinobrien1>
2017-06-22 13:26:58 -0700

    Added photos

--- source/view/javascript/place-init.js
+++ source/view/javascript/place-init.js
@@ -5,8 +5,30 @@ var testPlace = {
 	img: "https://media-cdn.tripadvisor.com/media/photo-s/03/4b/c9/dd/flying-goat-coffee.jpg"
 }
 
-function initPlacePanel(placeObj) {
+function makePanel(image, name, address, rating, type, price, isOpen) {
+	return `
+	<div class="place container" id="last">
+			<div class="row">
+				<div class="col-lg-3 image">
+					<img src=${image} alt="">
+				</div>
+				<div class="col-lg-5 summary">
+					<h1>${name}</h1>
+					<h3>${address}</h3>
+				</div>
+				<div class="col-lg-4 reviews">
+					<h2>Avg Rating:  ${rating}</h2>
+					<h3>Type: ${type}</h3>
+					<h3>Pricing: ${price}</h3>
+					<h3>Open Now: ${isOpen}</h3>
+				</div>
+			</div>
+		</div>
+	`
+}
 
+function initPlacePanel(placeObj) {
+	var price = "";
 	var photo_reference = placeObj.photos[0].photo_reference;
 	var keyParem = "&key=AIzaSyC_KZyErDtZ42CuFscO2l5YseWaV8MCHrQ";
 	var image = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=" + photo_reference + keyParem;
@@ -20,33 +42,13 @@ function initPlacePanel(placeObj) {
 	}
 
 	if (placeObj.price_level) {
-		var price = "";
 		for (var i = 1; i <= placeObj.price_level; i++) {
 			price += "$";
 		}
-		placeObj.price_level = price;
 	}
 	else {
 		placeObj.price_level = "N/A";
 	}
 
-	return `
-	<div class="place container" id="last">
-			<div class="row">
-				<div class="col-lg-3 image">
-					<img src=${image} alt="">
-				</div>
-				<div class="col-lg-5 summary">
-					<h1>${placeObj.name}</h1>
-					<h3>${placeObj.formatted_address}</h3>
-				</div>
-				<div class="col-lg-4 reviews">
-					<h2>Avg Rating:  ${placeObj.rating}</h2>
-					<h3>Type: ${placeObj.types[0]}</h3>
-					<h3>Pricing: ${placeObj.price_level}</h3>
-					<h3>Open Now: ${placeObj.opening_hours.open_now}</h3>
-				</div>
-			</div>
-		</div>
-	`
+	return makePanel(image, placeObj.name, placeObj.formatted_address, placeObj.rating, placeObj.types[0], price, placeObj.opening_hours.open_now);
 }
